0e7c1e8a6ada027b50e7a2db2a603c20acb6184f
sachinjat2802 <sachinjat2802@gmail.com>
2026-08-20 12:12:52 +0530

    chore: remove build artifacts from example app and update gitignore

--- my_first_polyglot_app/target/debug/deps/my_first_polyglot_app.d
+++
@@ -1,5 +0,0 @@
-D:\cpm\my_first_polyglot_app\target\debug\deps\my_first_polyglot_app.d: src\main.rs
-
-D:\cpm\my_first_polyglot_app\target\debug\deps\my_first_polyglot_app.exe: src\main.rs
-
-src\main.rs:
--- my_first_polyglot_app/target/debug/my_first_polyglot_app.d
+++
@@ -1,1 +0,0 @@
-D:\cpm\my_first_polyglot_app\target\debug\my_first_polyglot_app.exe: D:\cpm\my_first_polyglot_app\src\main.rs
